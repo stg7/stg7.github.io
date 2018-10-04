6a06db4d7ac5016ff34a8fb8ee517a5713380ed0
Steve <stg7@gmx.de>
2018-10-04 03:49:57 +0200

    reduce quality of large photsos

--- prepare_photos.sh
+++ prepare_photos.sh
@@ -6,6 +6,6 @@ mkdir -p photos/small/
 for x in photos/input/*; do
     echo $x..
     bn=$(basename $x)
-    convert $x -resize 2160 -quality 85 "photos/large/$bn"
-    convert $x -resize "200x200^" -gravity center -crop 200x200+0+0 +repage -quality 85 "photos/small/$bn"
+    convert $x -resize 2160 -quality 70 "photos/large/$bn"
+    convert $x -resize "200x200^" -gravity center -crop 200x200+0+0 +repage -quality 70 "photos/small/$bn"
 done
